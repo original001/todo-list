e10907b2d5a398b9799594d2c08fd0774552a5f3
Vladimir Tolstikov <wertyxa@bk.ru>
2017-06-04 03:46:26 +0500

    add test

---
+++ __tests__/test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import {Item, Check} from '../src/views/App.st.js';
+
+describe('', () => {
+  it('', () => {
+    const wrapper = shallow(<Item/>);
+    expect(wrapper.exists()).toBe(true);
+    // wrapper.simulate('click');
+    // const check = wrapper.find(Check);
+    // expect()
+  })
+})
