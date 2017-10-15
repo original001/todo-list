225c84b552cd4475cb584106cee6064f9232d8bc
Vladimir Tolstikov <wertyxa@bk.ru>
2017-10-15 22:45:33 +0500

    redesigned item

--- src/views/Item.jsx
+++ src/views/Item.jsx
@@ -2,6 +2,12 @@
 import React from 'react';
 import styled from 'styled-components';
 import {sheetPadding} from '../styled-variables';
+const startIcon = require('../img/start.svg');
+const stopIcon = require('../img/stop.svg');
+const crossIcon = require('../img/cross.svg');
+const crossHoverIcon = require('../img/cross-hover.svg');
+const checkIcon = require('../img/check.svg');
+const checkHoverIcon = require('../img/check-hover.svg');
 
 type Props = {
   checked: boolean,
@@ -16,67 +22,69 @@ export class Item extends React.Component {
   render() {
     const props = this.props;
     return (
-      <ItemWrapper onClick={this.props.onCheck}>
-        <Check checked={this.props.checked}/>
-        <TextItem>{props.children}</TextItem>
+      <ItemWrapper>
+        <Action />
+        <TextItem checked={props.checked}>{props.children}</TextItem>
+        {props.checked ||
+          <Check onClick={this.props.onCheck}/>
+        }
         <Close onClick={this.props.onClose}/>
       </ItemWrapper>
     )
   }
 }
 
-export const Check = styled.div`
-  float: left;
-  height: 30px;
-  width: 30px;
-  line-height: 30px;
-  border-radius: 15px;
-  border: 1px solid #ddd;
-  margin: 5px 0;
+
+const Action = styled.div`
+  display: inline-block;
+  height: 24px;
+  line-height: 24px;
+  width: 24px;
   text-align: center;
-  &:after {
-    font-family: FontAwesome;
-    font-weight: normal;
-    font-size: 16px;
-    content: ${props => props.checked ? '""' : ''};
-    color: green;
-  }
+  vertical-align: middle;
+  border-radius: 100%;
+  background: 50% 50% no-repeat;
+  background-image: url('${props => props.active ? stopIcon : startIcon}');
+  border: 1px solid #808080;
+  margin-right: 12px;
 `
 
 const TextItem = styled.div`
-  margin-left: 50px;
-  font-size: 16px;
+  display: inline-block;
+  font-size: 14px;
+  text-decoration: ${props => props.checked ? 'line-through' : 'none'};
 `
 
 const ItemWrapper = styled.div`
   position: relative;
-  padding: 10px ${sheetPadding};
+  padding: 0 ${sheetPadding};
   border-top: 1px solid #ddd;
-  height: 40px;
-  line-height: 40px;
+  line-height: 52px;
+`
+
+const RightIcon = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 16px;
+  height: 16px;
+  margin: auto;
   cursor: pointer;
+`
+
+const Close = styled(RightIcon)`
+  right: 20px;
+  background: url('${crossIcon}') 0 0 no-repeat;
   &:hover {
-    background: #f1f1f1;
+    background-image: url('${crossHoverIcon}');
   }
 `
 
-export const Close = styled.div`
-  position: absolute;
-  right: ${sheetPadding};
-  top: 0;
-  bottom: 0;
-  width: 30px;
-  height: 40px;
-  text-align: right;
-  margin: auto;
-  &:after {
-    font-family: FontAwesome;
-    font-weight: normal;
-    font-size: 16px;
-    color: #888;
-    content: '';
-  }
-  &:hover:after {
-    color: #444;
+const Check = styled(RightIcon)`
+  right: 50px;
+  width: 19px;
+  background: url('${checkIcon}') 0 0 no-repeat;
+  &:hover {
+    background-image: url('${checkHoverIcon}');
   }
 `
